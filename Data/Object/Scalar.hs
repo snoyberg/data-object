6d5676ef011913ad32564a3820c413bb00902fb8
Michael Snoyman <michael@snoyman.com>
2009-11-13 02:17:06 +0200

    Converted Data.Object.Raw to Text

--- Data/Object/Scalar.hs
+++ Data/Object/Scalar.hs
@@ -22,7 +22,7 @@ import Data.Text.Lazy (Text)
 import Data.Text.Lazy.Encoding (encodeUtf8)
 import Data.Time.Clock (UTCTime)
 import Data.Object
-import Data.Object.Raw
+import Data.Object.Text ()
 import System.Locale (defaultTimeLocale)
 import Data.Time.Format (formatTime)
 import Data.Attempt
@@ -37,9 +37,9 @@ data Scalar = Numeric   Rational
 
 type ScalarObject = Object String Scalar
 
-instance ConvertAttempt Scalar Raw where
+instance ConvertAttempt Scalar Text where
     convertAttempt = return . convertSuccess
-instance ConvertSuccess Scalar Raw where
+instance ConvertSuccess Scalar Text where
     convertSuccess (Numeric n) = convertSuccess $ show n
     convertSuccess (Text t) = convertSuccess $ encodeUtf8 t
     convertSuccess (Binary b) = convertSuccess b
---
+++ Data/Object/Text.hs
@@ -0,0 +1,199 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+---------------------------------------------------------
+--
+-- Module        : Data.Object.Text
+-- Copyright     : Michael Snoyman
+-- License       : BSD3
+--
+-- Maintainer    : Michael Snoyman <michael@snoyman.com>
+-- Stability     : Stable
+-- Portability   : portable
+---------------------------------------------------------
+
+-- FIXME should the convertible instances be moved to convertible?
+
+-- | Keys and values are lazy 'LT.Text's.
+module Data.Object.Text
+    ( TextObject
+    , toTextObject
+    , fromTextObject
+    ) where
+
+import Data.Object
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.Encoding as LTE
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString as BS
+import Data.ByteString.Class
+import Data.Time.Calendar
+import Safe (readMay)
+import Control.Monad ((<=<))
+import Data.Ratio (Ratio)
+import Data.Attempt
+import Data.Generics
+import Control.Exception (Exception)
+import Data.Convertible
+import qualified Safe.Failure as SF
+
+-- | 'Object's with keys and values of type 'LT.Text'.
+type TextObject = Object LT.Text LT.Text
+
+-- lazy bytestrings
+instance ConvertSuccess BL.ByteString LT.Text where
+    convertSuccess = LTE.decodeUtf8
+instance ConvertAttempt BL.ByteString LT.Text where
+    convertAttempt = return . convertSuccess
+
+instance ConvertSuccess LT.Text BL.ByteString where
+    convertSuccess = LTE.encodeUtf8
+instance ConvertAttempt LT.Text BL.ByteString where
+    convertAttempt = return . convertSuccess
+
+instance ToObject BL.ByteString a LT.Text where
+    toObject = scalarToObject
+instance FromObject BL.ByteString a LT.Text where
+    fromObject = scalarFromObject
+
+-- strict bytestrings
+instance ConvertSuccess BS.ByteString LT.Text where
+    convertSuccess = LTE.decodeUtf8 . toLazyByteString
+instance ConvertSuccess LT.Text BS.ByteString where
+    convertSuccess = fromLazyByteString . LTE.encodeUtf8
+instance ConvertAttempt BS.ByteString LT.Text where
+    convertAttempt = return . convertSuccess
+instance ConvertAttempt LT.Text BS.ByteString where
+    convertAttempt = return . convertSuccess
+instance ToObject BS.ByteString a LT.Text where
+    toObject = scalarToObject
+instance FromObject BS.ByteString a LT.Text where
+    fromObject = scalarFromObject
+
+-- Chars (and thereby strings)
+-- Extra complication since we're avoiding overlapping instances.
+class ListToRaw a where
+    listToRaw :: [a] -> LT.Text
+instance ListToRaw a => ConvertAttempt [a] LT.Text where
+    convertAttempt = return . convertSuccess
+instance ListToRaw a => ConvertSuccess [a] LT.Text where
+    convertSuccess = listToRaw
+instance ListToRaw Char where
+    listToRaw = LT.pack
+
+class ListFromRaw a where
+    listFromRaw :: MonadFailure ExpectedSingleCharacter m => LT.Text -> m [a] -- FIXME rather ugly
+instance ListFromRaw a => ConvertAttempt LT.Text [a] where
+    convertAttempt = listFromRaw
+instance ListFromRaw Char where
+    listFromRaw = return . LT.unpack
+
+data ExpectedSingleCharacter = ExpectedSingleCharacter String
+    deriving (Show, Typeable)
+instance Exception ExpectedSingleCharacter
+
+instance ToObject Char LT.Text LT.Text where
+    toObject c = Scalar $ LT.pack [c]
+    listToObject = Scalar . LT.pack
+instance FromObject Char LT.Text LT.Text where
+    fromObject = helper . LT.unpack <=< getScalar where
+        helper [x] = return x
+        helper x = failure $ ExpectedSingleCharacter x
+    listFromObject = fmap LT.unpack . getScalar
+
+-- Day
+instance ConvertSuccess Day LT.Text where
+    convertSuccess = LT.pack . show
+instance ConvertAttempt Day LT.Text where
+    convertAttempt = return . convertSuccess
+instance ToObject Day k LT.Text where
+    toObject = scalarToObject
+instance ConvertAttempt LT.Text Day where
+    convertAttempt t = do -- FIXME
+        let s = LT.unpack t
+        if length s /= 10
+            then failureString ("Invalid day: " ++ s)
+            else do
+                let x = do
+                    y' <- readMay $ take 4 s
+                    m' <- readMay $ take 2 $ drop 5 s
+                    d' <- readMay $ take 2 $ drop 8 s
+                    return (y', m', d')
+                case x of
+                    Just (y, m, d) -> return $ fromGregorian y m d
+                    Nothing -> failureString $ "Invalid day: " ++ s
+instance FromObject Day k LT.Text where
+    fromObject = scalarFromObject
+
+-- Bool
+instance ConvertAttempt Bool LT.Text where
+    convertAttempt = return . convertSuccess
+instance ConvertSuccess Bool LT.Text where
+    convertSuccess b = LT.pack $ if b then "true" else "false"
+instance ToObject Bool k LT.Text where
+    toObject = scalarToObject
+instance ConvertAttempt LT.Text Bool where
+    convertAttempt t =
+        case LT.unpack t of
+            -- list comes from http://yaml.org/type/bool.html
+            "y" -> return True
+            "Y" -> return True
+            "yes" -> return True
+            "Yes" -> return True
+            "YES" -> return True
+            "true" -> return True
+            "True" -> return True
+            "TRUE" -> return True
+            "on" -> return True
+            "On" -> return True
+            "ON" -> return True
+
+            "n" -> return False
+            "N" -> return False
+            "no" -> return False
+            "No" -> return False
+            "NO" -> return False
+            "false" -> return False
+            "False" -> return False
+            "FALSE" -> return False
+            "off" -> return False
+            "Off" -> return False
+            "OFF" -> return False
+
+            x -> failureString $ "Invalid bool value: " ++ x
+instance FromObject Bool k LT.Text where
+    fromObject = scalarFromObject
+
+-- Int
+instance ConvertSuccess Int LT.Text where
+    convertSuccess = LT.pack . show
+instance ConvertAttempt Int LT.Text where
+    convertAttempt = return . convertSuccess
+instance ToObject Int k LT.Text where
+    toObject = scalarToObject
+instance ConvertAttempt LT.Text Int where
+    convertAttempt = SF.read . LT.unpack
+instance FromObject Int k LT.Text where
+    fromObject = scalarFromObject
+
+-- Rational
+instance ConvertSuccess (Ratio Integer) LT.Text where
+    convertSuccess = LT.pack . show
+instance ConvertAttempt (Ratio Integer) LT.Text where
+    convertAttempt = return . convertSuccess
+instance ToObject (Ratio Integer) k LT.Text where
+    toObject = scalarToObject
+instance ConvertAttempt LT.Text (Ratio Integer) where
+    convertAttempt = SF.read . LT.unpack
+instance FromObject (Ratio Integer) k LT.Text where
+    fromObject = scalarFromObject
+
+-- | 'toObject' specialized for 'TextObject's
+toTextObject :: ToObject a LT.Text LT.Text => a -> TextObject
+toTextObject = toObject
+
+-- | 'fromObject' specialized for 'TextObject's
+fromTextObject :: FromObject a LT.Text LT.Text => TextObject -> Attempt a
+fromTextObject = fromObject
